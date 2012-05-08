4c9ed004c101ef176958bdcf9bcea5abf0156319
pmeenan <patmeenan@gmail.com>
2012-05-08 15:31:15 -0400

    Initial release for the app store

--- src/background.js
+++ src/background.js
@@ -1,16 +1,52 @@
 var active = false;
-var blockList = new Array();
 var blockHosts = new Array();
+var whiteList = new Array();
 var detected_spof = {};
 var badges = {};
 var tab_icons = {};
 
-var SPOF_LIST = 'http://spof.webpagetest.org/';
+// known 3rd-party assets
+var blockList = [
+	'platform\.twitter\.com',
+	'connect\.facebook\.net',
+	'platform\.linkedin\.com',
+	'assets\.pinterest\.com',
+	'widgets\.digg\.com',
+	'.*\.addthis\.com',
+	'ajax\.googleapis\.com',
+	'code\.jquery\.com',
+	'cdn\.jquerytools\.org',
+	'apis\.google\.com',
+	'.*\.google-analytics\.com',
+	'.*\.chartbeat\.com',
+	'static\.chartbeat\.com',
+	'.*\.2o7\.net',
+	'.*\.revsci\.net',
+	'.*\.omtrdc\.net',
+	'b\.scorecardresearch\.com',
+	'cdn\.sailthru\.com',
+	'.*browserid\.org',
+	'ad\.doubleclick\.net',
+	'js\.adsonar\.com',
+	'ycharts\.com',
+	'.*\.googlecode\.com',
+	'.*\.gstatic\.com',
+	'.*\.quantserve\.com',
+	'.*\.brightcove\.com'
+];
+
 var ICON_ACTIVE = "active.png";
 var ICON_INACTIVE = "inactive.png";
 var ICON_BLOCKED = "blocked.png";
 var ICON_DETECTED = "detected.png";
 var ICON_DEFAULT = ICON_INACTIVE;
+
+// Array Remove - By John Resig (MIT Licensed)
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
 
 chrome.webRequest.onBeforeRequest.addListener(
   function(info) {
@@ -78,15 +114,12 @@ function BlockURL(url) {
 	// get the host name
 	var hostRegex = new RegExp('[^/]*//([^/]+)', 'im');
 	var host = url.match(hostRegex)[1].toString();
-	for (i = 0; i < blockList.length && !block; i++) {
-		var blockRegex = new RegExp(blockList[i], 'im');
-		if (blockRegex.test(host)) {
-			block = true;
-		}
-	}
-	for (i = 0; i < blockHosts.length && !block; i++) {
-		if (blockHosts[i] == host) {
-			block = true;
+	if (!isOnWhiteList(host)) {
+		block = IsOnBlockList(host);
+		for (i = 0; i < blockHosts.length && !block; i++) {
+			if (blockHosts[i] == host) {
+				block = true;
+			}
 		}
 	}
 
@@ -96,36 +129,31 @@ function BlockURL(url) {
 	return block;
 }
 
+function IsOnBlockList(host) {
+	var found = false;
+	for (i = 0; i < blockList.length && !found; i++) {
+		var blockRegex = new RegExp(blockList[i], 'im');
+		if (blockRegex.test(host)) {
+			found = true;
+		}
+	}
+	return found;
+}
+
 function UpdateBlockList() {
-	console.log('Updating blockList');
-	blockHostsStr = localStorage['hosts'];
+	console.log('Default block list:');
+	console.log(blockList);
+	var blockHostsStr = localStorage['hosts'];
 	if (blockHostsStr && blockHostsStr.length) {
 		blockHosts = JSON.parse(blockHostsStr);
 		console.log('Local block list:');
 		console.log(blockHosts);
 	}
-	
-	try {
-	  var xhr = new XMLHttpRequest();
-	  xhr.open('GET', SPOF_LIST, true);
-	  xhr.onreadystatechange = function() {
-			if (xhr.readyState != 4)
-				return;
-			if (xhr.status == 200) {
-				blockList = JSON.parse(xhr.responseText);
-				xhr.responseText = null;
-				console.log('Server block list:');
-				console.log(blockList);
-			}
-			xhr.onreadystatechange = null;
-			xhr.abort();
-	  };
-	  xhr.onerror = function() {
-		console.log('Got an XHR error!');
-	  };
-	  xhr.send();
-	} catch (err) {
-	  console.log('XHR Error: ' + err);
+	var whiteListStr = localStorage['whitelist'];
+	if (whiteListStr && whiteListStr.length) {
+		whiteList = JSON.parse(whiteListStr);
+		console.log('White List:');
+		console.log(whiteList);
 	}
 }
 
@@ -161,11 +189,76 @@ function onRequest(request, sender, sendResponse) {
 		var response = {isActive: active};
 		if (request['tab'] && detected_spof[request['tab']] != undefined) {
 			response['spof'] = detected_spof[request['tab']];
+			if (response['spof']['scripts'] != undefined) {
+				for( var i = 0; i < response['spof']['scripts'].length; i++) {
+					if (response['spof']['scripts'][i]['host'] != undefined) {
+						response['spof']['scripts'][i]['whitelist'] = isOnWhiteList(response['spof']['scripts'][i]['host']);
+					}
+				}
+			}
 		}
 		sendResponse(response);
+	} else if (request.msg == 'enable') {
+		active = true;
+		ICON_DEFAULT = ICON_ACTIVE;
+		chrome.browserAction.setIcon({path: ICON_DEFAULT});
+		if (chrome['browsingData'] != undefined) {
+			chrome.browsingData.removeCache({}, function(){});
+		}
+		sendResponse({});
+	} else if (request.msg == 'disable') {
+		active = false;
+		ICON_DEFAULT = ICON_INACTIVE;
+		chrome.browserAction.setIcon({path: ICON_DEFAULT});
+		sendResponse({});
+	} else if (request.msg == 'reset') {
+		blockHosts = new Array();
+		localStorage['hosts'] = JSON.stringify(blockHosts);
+		whiteList = new Array();
+		localStorage['whitelist'] = JSON.stringify(whiteList);
+		UpdateBlockList();
+	} else if (request.msg == 'wl_add' && request['host'] != undefined) {
+		addToWhiteList(request['host']);
+	} else if (request.msg == 'wl_remove' && request['host'] != undefined) {
+		removeFromWhiteList(request['host']);
 	}
 };
 chrome.extension.onRequest.addListener(onRequest);
+
+/*********************************************************************************
+**********************************************************************************
+**
+**	White list management
+**
+**********************************************************************************
+**********************************************************************************/
+function isOnWhiteList(host) {
+	var found = false;
+	for (i = 0; i < whiteList.length && !found; i++) {
+		if (whiteList[i] == host) {
+			found = true;
+		}
+	}
+	return found;
+}
+
+function addToWhiteList(host) {
+	if (!isOnWhiteList(host)) {
+		whiteList.push(host);
+		localStorage['whitelist'] = JSON.stringify(whiteList);
+	}
+}
+
+function removeFromWhiteList(host) {
+	var found = false;
+	for (i = 0; i < whiteList.length && !found; i++) {
+		if (whiteList[i] == host) {
+			whiteList.remove(i);
+			localStorage['whitelist'] = JSON.stringify(whiteList);
+			found = true;
+		}
+	}
+}
 
 /*********************************************************************************
 **********************************************************************************
@@ -175,7 +268,16 @@ chrome.extension.onRequest.addListener(onRequest);
 **********************************************************************************
 **********************************************************************************/
 function setSPOF(tab_id, spofHosts, spofScripts) {
-	SetBadge(tab_id, ICON_DETECTED);
+	// make sure we have hosts that aren't on the white list
+	var hostCount = 0;
+	for (var i = 0; i < spofHosts.length; i++) {
+		if (!isOnWhiteList(spofHosts[i])) {
+			hostCount++;
+		}
+	}
+	if (hostCount > 0) {
+		SetBadge(tab_id, ICON_DETECTED);
+	}
 	detected_spof[tab_id] = {hosts: spofHosts, scripts: spofScripts};
 	// add the hosts if we don't already know about them
 	var modified = false;
@@ -212,27 +314,27 @@ function spofAddArrayElement(arr, str) {
 	}
 }
 
-function spofAddScript(arr, host, script) {
+function spofAddScript(arr, host, script, position) {
 	var found = false;
 	for( var i = 0; i < arr.length && !found; i++) {
 		if (arr[i]['host'].toString() == host) {
-			arr[i]['scripts'].push(script);
+			arr[i]['scripts'].push({'script':script, 'position':position});
 			found = true;
 		}
 	}
 	if (!found) {
-		arr.push({'host':host, 'scripts':[script]});
+		arr.push({'host':host, 'scripts':[{'script':script, 'position':position}]});
 	}
 }
 
 function spofCheck(tab_id, url, pageText) {
 	// build a list of "safe" host names (anything where css or images were served)
-	var cssRegex = /<link [^>]*href[ =htps:"]+\/\/([^\/ "]+)\/[^>]+>/gi
-	var imgRegex = /<img [^>]*src[ =htps:"]+\/\/([^\/ "]+)\/[^>]+>/gi
-	var scriptRegex = /<script [^>]*src[ =htps:"]+\/\/([^\/ "]+)\/[^>]+>/gi
-	var hostRegex = /(href|src)[ =htps:"]+\/\/([^\/ "]+)\//i
-	var tldRegex = /[-\w]+\.(?:[-\w]+\.xn--[-\w]+|[-\w]{3,}|[-\w]+\.[-\w]{2})$/i
-	var asyncRegex = /async[ ]*=/i
+	var cssRegex = /<link [^>]*href[ =htps:"]+\/\/([^\/ "]+)\/[^>]+>/gi;
+	var imgRegex = /<img [^>]*src[ =htps:"]+\/\/([^\/ "]+)\/[^>]+>/gi;
+	var scriptRegex = /<script [^>]*src[ =htps:"]+\/\/([^\/ "]+)\/[^>]+>/gi;
+	var hostRegex = /(href|src)[ =htps:"]+\/\/([^\/ "]+)\//i;
+	var tldRegex = /[-\w]+\.(?:[-\w]+\.xn--[-\w]+|[-\w]{3,}|[-\w]+\.[-\w]{2})$/i;
+	var asyncRegex = /async[ ]*=/i;
 	var safeTLDs = new Array();
 	var thirdParty = new Array();
 	var spofHosts = new Array();
@@ -256,23 +358,22 @@ function spofCheck(tab_id, url, pageText) {
 		}
 	}
 	console.log("Safe TLD's: " + safeTLDs);
-	matches = pageText.match(scriptRegex);
-	if (matches) {
-		for (var i = 0; i < matches.length; i++) {
-			try {
-				var script = matches[i].toString();
-				var host = script.match(hostRegex)[2].toString();
-				var tld = host.match(tldRegex).toString();
-				if (!spofMatch(safeTLDs, tld)) {
-					spofAddArrayElement(thirdParty, host);
-					if (!asyncRegex.test(script)) {
-						spofAddArrayElement(spofHosts, host);
-						spofAddScript(spofScripts, host, script);
-						console.log('SPOF script: ' + script);
-					}
+	var pageLen = pageText.length;
+	while ((match = scriptRegex.exec(pageText)) != null) {
+		try {
+			var position = (match.index / pageLen) * 100;
+			var script = match[0].toString();
+			var host = script.match(hostRegex)[2].toString();
+			var tld = host.match(tldRegex).toString();
+			if (IsOnBlockList(host) || !spofMatch(safeTLDs, tld)) {
+				spofAddArrayElement(thirdParty, host);
+				if (!asyncRegex.test(script)) {
+					spofAddArrayElement(spofHosts, host);
+					spofAddScript(spofScripts, host, script, position);
+					console.log('SPOF script (' + position.toFixed(2) + '%): ' + script);
 				}
-			} catch(err) {}
-		}
+			}
+		} catch(err) {}
 	}
 	console.log("SPOF Hosts: " + spofHosts);
 	if (spofHosts.length) {
--- src/popup.js
+++ src/popup.js
@@ -3,23 +3,85 @@ try {
 var tabId = document.URL.match(/tab=([0-9]+)/)[1].toString();
 } catch (err) {
 }
-chrome.extension.sendRequest({msg: 'getSPOF', tab: tabId}, function(response) {
-	var html = '';
-	if (response['isActive']) {
-		html += '<h1>Resource blocking is currently Active</h1>';
-	} else {
-		html += '<h1>Resource blocking is currently Disabled</h1>';
-	}
-	if (response['spof'] != undefined) {
-		html += '<hr><h1>Possible Frontend SPOF from:</h1><ul class="hosts">';
-		for (var i = 0; i < response['spof'].scripts.length; i++) {
-			html += '<li>' + response['spof'].scripts[i].host + '<ul class="scripts">';
-			for (var j = 0; j < response['spof'].scripts[i].scripts.length; j++) {
-				html += '<li>' + response['spof'].scripts[i].scripts[j].replace(/>/g, '&gt;').replace(/</g, '&lt;') + '</li>';
-			}
-			html += '</ul></li>'
+
+function spofUpdate() {
+	chrome.extension.sendRequest({msg: 'getSPOF', tab: tabId}, function(response) {
+		var html = '';
+		if (response['isActive']) {
+			html += '<h1>Resource blocking is currently Active <button class="disable">Disable</button>';
+		} else {
+			html += '<h1>Resource blocking is currently Disabled <button class="enable">Enable</button>';
 		}
-		html += '</ul>';
-	}
-	document.getElementById('content').innerHTML = html;
-});
+		html += '<button class="reset">Reset</button></h1>'
+		if (response['spof'] != undefined) {
+			html += '<hr><h1>Possible Frontend SPOF from:</h1><ul class="hosts">';
+			for (var i = 0; i < response['spof'].scripts.length; i++) {
+				var host = response['spof'].scripts[i].host;
+				if (response['spof'].scripts[i].whitelist) {
+					html += '<li class="whitelist"><span class="host">' + host + '</span> - whitelisted <button host="' + host + '" class="remove">Remove</button>';
+				} else {
+					html += '<li><span class="host">' + host + '</span> - <button host="' + host + '" class="add">Whitelist</button>';
+				}
+				html += '<ul class="scripts">';
+				for (var j = 0; j < response['spof'].scripts[i].scripts.length; j++) {
+					var position = response['spof'].scripts[i].scripts[j].position.toFixed(0);
+					var group = Math.floor(position / 10);
+					var script = response['spof'].scripts[i].scripts[j].script.replace(/>/g, '&gt;').replace(/</g, '&lt;');
+					html += '<li class="pos' + group + '"> (<b>' + position + '</b>%) - ' + script + '</li>';
+				}
+				html += '</ul></li>'
+			}
+			html += '</ul>';
+		}
+		document.getElementById('content').innerHTML = html;
+
+		// attach the event handlers
+		var buttons = document.querySelectorAll("button.enable");
+		for (var i = 0, length = buttons.length; i < length; i++) {
+				buttons[i].addEventListener('click', spofEnable);
+		}
+		buttons = document.querySelectorAll("button.disable");
+		for (var i = 0, length = buttons.length; i < length; i++) {
+				buttons[i].addEventListener('click', spofDisable);
+		}
+		buttons = document.querySelectorAll("button.reset");
+		for (var i = 0, length = buttons.length; i < length; i++) {
+				buttons[i].addEventListener('click', spofReset);
+		}
+		var buttons = document.querySelectorAll("button.remove");
+		for (var i = 0, length = buttons.length; i < length; i++) {
+				buttons[i].addEventListener('click', spofRemoveWhitelist);
+		}
+		var buttons = document.querySelectorAll("button.add");
+		for (var i = 0, length = buttons.length; i < length; i++) {
+				buttons[i].addEventListener('click', spofAddWhitelist);
+		}
+	});
+}
+
+function spofDisable() {
+	chrome.extension.sendRequest({'msg': 'disable'}, function(response) {});
+	spofUpdate();
+}
+
+function spofEnable() {
+	chrome.extension.sendRequest({'msg': 'enable'}, function(response) {});
+	spofUpdate();
+}
+
+function spofReset() {
+	chrome.extension.sendRequest({'msg': 'reset'}, function(response) {});
+	spofUpdate();
+}
+
+function spofRemoveWhitelist(e) {
+	chrome.extension.sendRequest({'msg': 'wl_remove', 'host': e.target.getAttribute('host')}, function(response) {});
+	spofUpdate();
+}
+
+function spofAddWhitelist(e) {
+	chrome.extension.sendRequest({'msg': 'wl_add', 'host': e.target.getAttribute('host')}, function(response) {});
+	spofUpdate();
+}
+
+spofUpdate();
